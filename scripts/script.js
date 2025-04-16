2bffda2cbe61f3b4252932b200439019432d9a50
TimilehinOlajolo <olajolotaiwo2@gmail.com>
2025-04-16 13:33:18 +0100

    updated

--- scripts/script.js
+++ scripts/script.js
@@ -7,17 +7,4 @@ document.addEventListener("DOMContentLoaded", () => {
   menuToggle.addEventListener("click", () => {
       sidebar.classList.toggle("open");
   });
-
-  // Dark mode toggle
-  darkModeToggle.addEventListener("click", () => {
-      document.body.classList.toggle("dark-mode");
-      
-      if (document.body.classList.contains("dark-mode")) {
-          document.body.style.background = "#222";
-          document.body.style.color = "white";
-      } else {
-          document.body.style.background = "#f4f4f4";
-          document.body.style.color = "black";
-      }
-  });
 });
